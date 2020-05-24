bd62fbdc9e2b793c9c4854046bd847dc341f2272
Shahid Foy <shahidfoy@gmail.com>
2020-05-23 23:42:26 -0600

    added availibility service

--- src/task-manager/models/available.model.ts
+++ src/task-manager/models/available.model.ts
@@ -2,17 +2,15 @@ import * as mongoose from 'mongoose';
 
 export const AvailableSchema = new mongoose.Schema({
     userId: { type: String, defulat: '' },
-    dateRange: [{ type: Date, default: Date.now() }],
+    date: { type: Date, default: Date.now() },
     startTime: { type: String, default: '' },
     endTime: { type: String, default: '' },
-    includedDayIndex: [ { type: Number } ]
 });
 
 export interface Available extends mongoose.Document {
     _id: string;
     userId: string;
-    dateRange: Array<Date>;
+    date: Date;
     startTime: string;
     endTime: string;
-    inculdeDayIndex: Array<number>;
 }
--- src/task-manager/task-manager.service.ts
+++ src/task-manager/task-manager.service.ts
@@ -12,6 +12,57 @@ export class TaskManagerService {
         @InjectModel('Available') private readonly availableModel: Model<Available>,
     ) {}
 
+    async addAvailabile(userId: string, dateRange: DateRange, startTime: string, endTime: string, includedDayIndex: Array<number>): Promise<Partial<any>> {
+        const startDate = new Date(dateRange.startDate);
+        const endDate = new Date(dateRange.endDate);
+
+        if (!dateRange.startDate) {
+            throw new InternalServerErrorException({ message: 'starting date not set.' });
+        }
+
+        if (!dateRange.endDate) {
+            const available: Partial<Available> = {
+                userId,
+                date: startDate,
+                startTime,
+                endTime,
+            }
+    
+            return await this.taskModel.create(available).then(async (newAvailable: Task) => {
+                return { message: 'task created' };
+            }).catch(err => { 
+                throw new InternalServerErrorException({ message: `Error creating availability ${err}`}); 
+            });
+        } else {
+            // double check this
+            const lengthOfDays = endDate.getDate() - startDate.getDate() + 1;
+            // console.log(lengthOfDays);
+            let availabilityCountsCreated = 0;
+
+            for (let i = 0; i < lengthOfDays; i++) {
+                const date = new Date(startDate);
+                date.setDate(date.getDate() + i);
+
+                const dateIncluded = includedDayIndex.filter(dayIndex => dayIndex === date.getDay());
+                if (dateIncluded.length === 1) {
+                    const available: Partial<Available> = {
+                        userId,
+                        date,
+                        startTime,
+                        endTime,
+                    }
+
+                    await this.taskModel.create(available).then(async (newAvailable: Task) => {
+                        availabilityCountsCreated++;
+                    }).catch(err => { 
+                        throw new InternalServerErrorException({ message: `Error setting availability ${err}`}); 
+                    });
+                }
+            }
+            return { message: `${availabilityCountsCreated} availbility status created` };
+        }
+    }
+
     async addTask(userId: string, taskName: string, dateRange: DateRange, startTime: string, endTime: string, includedDayIndex: Array<number>): Promise<Partial<any>> {
         const startDate = new Date(dateRange.startDate);
         const endDate = new Date(dateRange.endDate);
@@ -30,7 +81,6 @@ export class TaskManagerService {
             }
     
             return await this.taskModel.create(task).then(async (newTask: Task) => {
-                const response = { message: 'Task Created Successfully' };
                 return { message: 'task created' };
             }).catch(err => { 
                 throw new InternalServerErrorException({ message: `Error creating task ${err}`}); 
